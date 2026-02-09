ce7938b079f49b6cdecceea3e0fc4740e2937188
SamaniegoM <dsamaniegom@edu.tecnocampus.cat>
2026-02-09 16:32:07 +0100

    initialPush

---
+++ TeslaInLove/Source/TeslaInLove/TeslaInLove.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TeslaInLove.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeslaInLove, "TeslaInLove" );
