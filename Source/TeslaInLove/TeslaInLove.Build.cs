310ceb85139953b06ffab0ca7d79ada7c7b0166c
Zad826 <158450684+Zad826@users.noreply.github.com>
2026-02-24 19:15:24 +0100

    New Project Config

--- Source/TeslaInLove/TeslaInLove.Build.cs
+++ Source/TeslaInLove/TeslaInLove.Build.cs
@@ -7,37 +7,14 @@ public class TeslaInLove : ModuleRules
 	public TeslaInLove(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PublicDependencyModuleNames.AddRange(new string[] {
-			"Core",
-			"CoreUObject",
-			"Engine",
-			"InputCore",
-			"EnhancedInput",
-			"AIModule",
-			"NavigationSystem",
-			"StateTreeModule",
-			"GameplayStateTreeModule",
-			"Niagara",
-			"UMG",
-			"Slate"
-		});
-
-		PrivateDependencyModuleNames.AddRange(new string[] { });
-
-		PublicIncludePaths.AddRange(new string[] {
-			"TeslaInLove",
-			"TeslaInLove/Variant_Strategy",
-			"TeslaInLove/Variant_Strategy/UI",
-			"TeslaInLove/Variant_TwinStick",
-			"TeslaInLove/Variant_TwinStick/AI",
-			"TeslaInLove/Variant_TwinStick/Gameplay",
-			"TeslaInLove/Variant_TwinStick/UI"
-		});
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-
+		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
